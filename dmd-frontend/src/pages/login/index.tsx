40106706ff2a9e5f4592168eedf60d22b5b20913
Mark <anonymous2438@gmail.com>
2020-08-28 09:54:13 +0700

    Init login form UI

--- dmd-frontend/src/pages/login/index.tsx
+++ dmd-frontend/src/pages/login/index.tsx
@@ -3,57 +3,151 @@ import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
-const emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+/*
+ *  Must contains:
+ *    - from 8 to 32 characters
+ *    - at least 1 Uppercase character
+ *    - at least 1 lowercase character
+ *    - at least 1 numberic character
+ *    - at least 1 special character
+ * */
+const passwordReg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]|[_]).{8,32}$/;
+const usernameReg = /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
 
 const Login: React.FC<any> = (props: any) => {
     const { handleSubmit, register, errors, setValue } = useForm<{
-        email: string,
+        username: string,
+        password: string,
     }>({
         defaultValues: {
-            email: '',
+            username: '',
+            password: '',
         }
     });
 
-    const _handleFormSubmit = useCallback((values) => {
-        console.log(values);
+    const _handleFormSubmit = useCallback((data, e) => {
+        e.preventDefault();
+        console.log(data);
     }, []);
 
-    const _handleEmailChange = useCallback((e, email) => {
-        setValue('email', email);
-    }, []);
+    const _handleUsernameChange = useCallback((e) => {
+        setValue('username', e.target.value);
+    }, [setValue]);
+
+    const _handlePasswordChange = useCallback((e) => {
+        setValue('password', e.target.value);
+    }, [setValue]);
 
     useEffect(() => {
-        register('email', {
+        register('username', {
             validate: (value) => {
-                if (value.length === 0) return 'Email cannot be empty';
-               return emailReg.test(value) || 'Invalid email format';
+                if (value.length === 0) return 'Username cannot be empty';
+                return usernameReg.test(value) || 'Invalid username format';
+            }
+        });
+        register('password', {
+            validate: (value) => {
+                if (value.length === 0) return 'Password cannot be empty';
+                if (value.length < 8 || value.length > 32) return 'Password must be between 8 and 32 characters';
+                return passwordReg.test(value) || 'Invalid password, please have a look at our password policy';
             }
         });
     }, [register]);
 
     return (
-        <FlexContainer maxWidth="md">
-            <Grid container>
-                <Grid item>
-                    <form onSubmit={_handleFormSubmit}>
-                        <TextField
-                            name="email"
-                            error={errors?.email}
-                            helperText={errors?.email?.message}
-                            onChange={_handleEmailChange}
-                        /> 
-                    </form>
+        <FlexContainer
+            maxWidth="md"
+        >
+            <Grid
+                container
+                justify="center"
+                alignItems="center"
+            >
+                <Grid
+                    item
+                    xs={12}
+                    sm={12}
+                    md={6}
+                >
+                    <StyledPaper
+                        elevation={3}
+                    >
+                        <Grid
+                            container
+                            direction="column"
+                        >
+                            <Grid
+                                item
+                            >
+                                <LoginFormTitle variant="h3">LOGIN</LoginFormTitle>
+                            </Grid>
+                            <Grid item>
+                             <StyledForm onSubmit={handleSubmit(_handleFormSubmit)}>
+                                 <StyledTextField
+                                     name="username"
+                                     label="Username"
+                                     ref={register}
+                                     error={Boolean(errors?.username)}
+                                     onChange={_handleUsernameChange}
+                                     helperText={errors?.username?.message}
+                                 /> 
+                                 <StyledTextField
+                                     name="password"
+                                     type="password"
+                                     label="Password"
+                                     ref={register}
+                                     error={Boolean(errors?.password)}
+                                     onChange={_handlePasswordChange}
+                                     helperText={errors?.password?.message}
+                                 />
+                                 <Button
+                                     type="submit"
+                                     color="primary"
+                                     variant="contained"
+                                 >
+                                     Login
+                                 </Button>
+                             </StyledForm>
+                            </Grid>
+                       </Grid>
+                    </StyledPaper>
                 </Grid>
             </Grid>
-        </FlexContainer>
+         </FlexContainer>
     );
 };
 
+const LoginFormTitle = styled(Typography)`
+    padding: 20px 0;
+`;
+
+const StyledPaper = styled(Paper)`
+    width: 100%;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
 const FlexContainer = styled(Container)`
     display: flex;
+    height: 90vh;
+`;
+
+
+const StyledTextField = styled(TextField)`
+    margin-bottom: 20px;
+`;
+
+const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
 `;
 
 export default Login;
