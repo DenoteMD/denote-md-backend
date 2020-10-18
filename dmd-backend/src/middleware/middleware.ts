3f9f541dc56f6892e2d6a17163b0e83cf1fc150d
Chiro Hiro <chiro8x@gmail.com>
2020-10-18 15:10:17 +0700

    Fix middleware parse wrong input

--- dmd-backend/src/middleware/middleware.ts
+++ dmd-backend/src/middleware/middleware.ts
@@ -20,7 +20,14 @@ Mux.use((req: express.Request, res: express.Response, next: NextFunction) => {
 
   req.on('end', () => {
     res.setHeader('Content-Type', 'application/json');
-    const contentLength = req.header('Content-Length');
+    const headerContentLength = req.header('Content-Length');
+    let contentLength = 0;
+    if (typeof headerContentLength === 'string') {
+      contentLength = parseInt(headerContentLength, 10);
+    }
+    if (typeof headerContentLength === 'number' && Number.isInteger(headerContentLength)) {
+      contentLength = headerContentLength;
+    }
     if (typeof contentLength === 'number' && Number.isInteger(contentLength)) {
       const { obj, signer } = DenoteUI.recoverObject(data.toString('utf8'));
       req.body = obj;
