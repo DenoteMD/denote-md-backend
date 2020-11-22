57cee3475a5b2f1142ade0a69c02b793a53536b8
Chiro Hiro <chiro8x@gmail.com>
2020-11-22 19:09:31 +0700

    Update model/schema for comment

--- src/model/comment.ts
+++ src/model/comment.ts
@@ -2,31 +2,56 @@ import mongoose, { Schema, Document } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
 /**
- * Document of a comment
+ * Document interface of comment
  * @export
  * @interface IDocumentComment
  * @extends {Document}
  */
 export interface IDocumentComment extends Document {
-  uuid: string;
+  uuid: String;
   author: Schema.Types.ObjectId;
+  articleId: Schema.Types.ObjectId;
+  reply: Schema.Types.ObjectId;
+  votedUser: Schema.Types.ObjectId[];
   content: String;
   created: Date;
   updated: Date;
   hidden: Boolean;
   vote: Number;
-  reply: Schema.Types.ObjectId;
+}
+
+/**
+ * This interface will be represent for Comment in our code
+ * @export
+ * @interface IComment
+ */
+export interface IComment {
+  uuid: string;
+  author: Schema.Types.ObjectId;
+  articleId: Schema.Types.ObjectId;
+  // If reply isn't set that meant this comment, is in the root of comment section
+  reply?: Schema.Types.ObjectId;
+  // We need to record whom had voted
+  votedUser: Schema.Types.ObjectId[];
+  content: string;
+  created: Date;
+  updated: Date;
+  hidden?: boolean;
+  // This value used to cache an show vote directly instead of calculate number of vote every time
+  vote: number;
 }
 
 export const SchemaComment = new Schema({
   uuid: { type: String, default: uuidv4(), unique: true, index: true },
   author: { type: Schema.Types.ObjectId, ref: 'User' },
+  articleId: { type: Schema.Types.ObjectId, ref: 'Article' },
+  reply: { type: Schema.Types.ObjectId, ref: 'Comment' },
+  votedUser: { type: [Schema.Types.ObjectId], ref: 'User' },
   content: String,
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now },
   hidden: Boolean,
   vote: { type: Number, default: 0 },
-  reply: { type: Schema.Types.ObjectId, ref: 'Comment' },
 });
 
 SchemaComment.pre<IDocumentComment>('save', function prevSavePost(next: mongoose.HookNextFunction) {
