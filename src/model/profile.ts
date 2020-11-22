0fe374c84a3724c24d94088a4da9c62eacb12e88
kevin <dqtkien@gmail.com>
2020-11-22 21:50:58 +0700

    update with new requirements

--- src/model/profile.ts
+++ src/model/profile.ts
@@ -6,27 +6,18 @@ import { v4 as uuidv4 } from 'uuid';
  * @interface IDocumentProfile
  * @extends {Document}
  */
-
 export interface IDocumentProfile extends Document {
   uuid: String;
   firstName: String;
   lastName: String;
-  description: String;
-  updated: Date;
+  extraData: Array<Object>;
 }
 
 export const SchemaProfile = new Schema({
   uuid: { type: String, default: uuidv4(), unique: true, index: true },
   firstName: String,
   lastName: String,
-  description: String,
-  created: { type: Date, default: Date.now },
-  updated: { type: Date, default: Date.now },
-});
-
-SchemaProfile.pre<IDocumentProfile>('save', function preSaveProfile(next: mongoose.HookNextFunction) {
-  this.updated = new Date();
-  next();
+  extraData: [Object],
 });
 
 export const ModelProfile = mongoose.model<IDocumentProfile>('Profile', SchemaProfile);
