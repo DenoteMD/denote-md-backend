d7026481830af9dd3c8cfac955c6bbf3617428f3
Chiro Hiro <chiro8x@gmail.com>
2020-11-29 18:05:49 +0700

    Update model that helped new authenticator

--- src/model/user.ts
+++ src/model/user.ts
@@ -8,29 +8,40 @@ import { v4 as uuidv4 } from 'uuid';
  * @extends {Document}
  */
 export interface IDocumentUser extends Document {
-  uuid: string;
-  alias: String;
-  status: String;
-  userId: String;
-  created: Date;
-  active: Boolean;
-  email: String;
   profileId: Schema.Types.ObjectId;
   deviceId: Schema.Types.ObjectId[];
+  uuid: String;
+  alias: String;
+  status: String;
+  created: Date;
+  active: Boolean;
+  achievement: [String];
+  email: String;
+}
+
+export interface IUser {
+  profileId: Schema.Types.ObjectId;
+  deviceId: Schema.Types.ObjectId[];
+  uuid: string;
+  alias: string;
+  status: string;
+  created: Date;
+  active: boolean;
+  achievement: [String];
+  email: string;
 }
 
 export const SchemaUser = new Schema({
+  profileId: { type: Schema.Types.ObjectId, ref: 'Profile' },
+  deviceId: { type: [Schema.Types.ObjectId], ref: 'User' },
   uuid: { type: String, default: uuidv4(), unique: true, index: true },
   alias: { type: String, unique: true, index: true },
   status: String,
   vote: { type: Number, default: 100 },
-  userId: { type: String, unique: true, index: true },
   created: { type: Date, default: Date.now },
   active: { type: Boolean, default: true },
   achievement: [String],
   email: { type: String, unique: true, index: true },
-  profileId: { type: Schema.Types.ObjectId, ref: 'Profile' },
-  deviceId: { type: [Schema.Types.ObjectId], ref: 'User' },
 });
 
 export const ModelUser = mongoose.model<IDocumentUser>('User', SchemaUser);
