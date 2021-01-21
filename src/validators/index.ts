0ce490e5e1b8278f355e332c16e71d400025609a
kevin <dqtkien@gmail.com>
2021-01-21 22:03:39 +0700

    Add some validations

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,17 +1,25 @@
 import { filterXSS } from 'xss';
-import { Validator } from '../framework/validator';
+import { IField, Validator } from '../framework/validator';
 
 const titleLength = 255;
 const bodyLength = 1048576;
+const limitPagi = 10;
+const defaultOffset = 0;
 
-export const UuidValidator = new Validator({
-  name: 'uuid',
-  location: 'params',
-  type: 'string',
-  require: true,
-  validator: (v: string) => /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(v),
-  message: `Invalid UUID value`,
-});
+export const createUuid = (name: string): IField => {
+  return {
+    name,
+    location: 'params',
+    type: 'string',
+    require: true,
+    validator: (v: string) => /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(v),
+    message: `Invalid UUID value`,
+  };
+};
+
+export const UuidValidator = new Validator(createUuid('uuid'));
+export const ArticleUuidValidator = new Validator(createUuid('articleUuid'));
+export const CommentUuidValidator = new Validator(createUuid('commentUuid'));
 
 export const ArticleValidator = new Validator(
   {
@@ -42,41 +50,34 @@ export const ArticleValidator = new Validator(
   },
 );
 
-export const ArticleUuidValidator = new Validator({
-  name: 'articleUuid',
-  location: 'params',
+export const CommentValidator = new Validator(createUuid('uuid'), {
+  name: 'content',
+  location: 'body',
   type: 'string',
   require: true,
-  validator: (v: string) => /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89AB][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v),
-  message: `Invalid UUID value`,
+  validator: (v: string) => v.length < bodyLength,
+  postProcess: (v: string) => filterXSS(v),
+  message: `Body should contain less than ${bodyLength}`,
 });
 
-export const CommentUuidValidator = new Validator({
-  name: 'commentUuid',
-  location: 'params',
-  type: 'string',
-  require: true,
-  validator: (v: string) => /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89AB][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v),
-  message: `Invalid UUID value`,
-});
-
-export const CommentValidator = new Validator(
+export const PaginationValidator = new Validator(
   {
-    name: 'uuid',
-    location: 'params',
-    type: 'string',
+    name: 'limit',
+    location: 'body',
+    type: 'number',
+    defaultValue: limitPagi,
     require: true,
-    validator: (v: string) => /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89AB][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v),
-    message: `Invalid UUID value`,
+    validator: (v: number) => /^[1-9]+[0-9]*$/.test(v.toString()) && v <= limitPagi,
+    message: 'Invalid limit number',
   },
   {
-    name: 'content',
+    name: 'offset',
     location: 'body',
-    type: 'string',
+    type: 'number',
+    defaultValue: defaultOffset,
     require: true,
-    validator: (v: string) => v.length < bodyLength,
-    postProcess: (v: string) => filterXSS(v),
-    message: `Body should contain less than ${bodyLength}`,
+    validator: (v: number) => /^[1-9]+[0-9]*$/.test(v.toString()),
+    message: 'Invalid offset number',
   },
 );
 
