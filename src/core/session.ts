78ad32050a87de206c694556c3d73c2a0aa0fedf
Chiro Hiro <chiro8x@gmail.com>
2021-02-10 14:15:41 +0700

    Add session to core

---
+++ src/core/session.ts
@@ -0,0 +1,54 @@
+import { IDocumentSession } from '../model/session';
+import { IDocumentUser } from '../model/user';
+
+/**
+ * Is b a subset of a?
+ * @param {any[]} a
+ * @param {any[]} b
+ */
+function isSubset(a: any[], b: any[]) {
+  return b.every((ib) => a.includes(ib));
+}
+
+export class DenoteSession {
+  private sessionData: Map<string, any>;
+
+  constructor(sessionDocument: IDocumentSession) {
+    this.sessionData = new Map<string, any>();
+    // Session data block
+    this.sessionData.set('session-data', sessionDocument);
+    this.sessionData.set('user-data', sessionDocument.user);
+  }
+
+  /**
+   * Makre sure user authenticated and there are no data mismatch
+   * @return {*}
+   * @memberof DenoteSession
+   */
+  public isAuthenticated() {
+    if (this.sessionData.get('user-data') && this.sessionData.get('session-data')) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Get session data
+   * @return {*}  {ISession}
+   * @memberof DenoteSession
+   */
+  public getSession(): IDocumentSession {
+    return this.sessionData.get('session-data');
+  }
+
+  /**
+   * Get user of this session
+   * @return {*}  {IUser}
+   * @memberof DenoteSession
+   */
+  public getUser(): IDocumentUser {
+    return this.sessionData.get('user-data');
+  }
+}
+
+export default DenoteSession;
